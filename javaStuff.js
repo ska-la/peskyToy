c5b7933b517a9c0f297cd6770df6dda4f9e1d35d
ska-la <ak.vokchus@gmail.com>
2016-10-27 12:54:07 +0300

    move javascript to separate file .js ..

---
+++ javaStuff.js
@@ -0,0 +1,72 @@
+/*------------------------ Global js place -----------------------*/
+"use strict";
+
+var curSlide = 0;
+var mI;
+
+w3IncludeHTML();
+
+function hw02SlideShow() {
+  var i;
+  var es = document.getElementsByClassName("hwRev02");
+  var es_m = document.getElementsByClassName("hwRev02m");
+  var es_s = document.getElementsByClassName("hwRev02s");
+/*  var bgs = document.getElementsByClassName("idBadges");*/
+  var bgs_m = document.getElementsByClassName("idM_Badges");
+  for ( i = 0; i < es.length; i++ ) {
+    es[i].style.display = "none";
+    es_m[i].style.display = "none";
+    es_s[i].style.display = "none";
+/*    bgs[i].className = bgs[i].className.replace(" w3-white","");*/
+    bgs_m[i].className = bgs_m[i].className.replace(" w3-white","");
+  }
+  if ( curSlide == es.length ) { curSlide = 0; }
+  es[curSlide].style.display = "block";
+  es_m[curSlide].style.display = "block";
+  es_s[curSlide].style.display = "block";
+/*  bgs[curSlide].className += " w3-white";*/
+  bgs_m[curSlide].className += " w3-white";
+  curSlide++;
+}
+
+function showImg(i) {
+  curSlide = i - 1;
+}
+
+mI = setInterval(hw02SlideShow, 4000);
+
+function openRightNav() {
+  document.getElementById("idRightNav").style.display = "block";
+}
+function closeRightNav() {
+  document.getElementById("idRightNav").style.display = "none";
+}
+
+function switchToMedium(strMC) {
+  var i;
+  var es = document.getElementsByClassName("myContMedium");
+  for ( i = 0; i < es.length; i++ ) {
+    es[i].style.display = "none";
+  }
+  document.getElementById(strMC).style.display = "block";
+}
+
+function switchTo(strMC) {
+  document.getElementById(strMC).style.display = "block";
+}
+
+function openMyAccord(strAcc) {
+  var e = document.getElementById(strAcc);
+  if ( e.className.indexOf("w3-show") == -1 ) {
+    e.className += " w3-show";
+    if ( strAcc == "idVideoSmall" ) {
+      document.getElementById("idVideoMedium").style.display = "block";
+    }
+  } else
+    e.className = e.className.replace(" w3-show", "");
+    if ( strAcc == "idVideoSmall" ) {
+      document.getElementById("idVideoMedium").style.display = "none";
+    }
+}
+
+
